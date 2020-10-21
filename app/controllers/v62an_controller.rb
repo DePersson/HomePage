47078e05904403cdabe0edfbf7e28f5ef3b12aa3
Dennis Persson <dennis.persson@ike.com>
2020-10-21 21:44:10 +0200

    Added remaining family

--- app/controllers/v62an_controller.rb
+++ app/controllers/v62an_controller.rb
@@ -1,8 +1,17 @@
 class V62anController < ApplicationController
 	def home
 	end
-		
+
+	def dennis
+	end
+
+	def fanny
+	end
+
 	def liam
 	end
-	
+		
+	def phy
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'v62an#home'
+  get '/dennis', to: "v62an#dennis"
+  get '/fanny', to: "v62an#fanny"
   get '/liam', to: "v62an#liam"
+  get '/phy', to: "v62an#phy"
   # verb		to: 'controller#action'
 end
